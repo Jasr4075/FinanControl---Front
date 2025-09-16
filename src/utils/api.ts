94fa148cf698f1705d3d28ee48e75bd8f03307a5
jasr4075 <jasr4075@gmail.com>
2025-09-16 11:04:28 +0000

    server atulizado para railway

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,8 +3,9 @@ import axios from "axios";
 import { getToken, getRefreshToken, saveToken, saveRefreshToken, saveUser, forceLogout } from "../features/auth/auth";
 
 const api = axios.create({
-  baseURL: "https://financontrol-y8zd.onrender.com/api",
+  // baseURL: "https://financontrol-y8zd.onrender.com/api",
   // baseURL: "http://localhost:3000/api",
+  baseURL: "https://financontrol-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
